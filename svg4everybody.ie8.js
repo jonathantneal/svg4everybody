db9df7c7758a5ed581e81700266fad0cf895e061
Steven Kinley <tseven@gmail.com>
2014-05-28 18:33:57 -0700

    Extending to allow external testing to depict fix type along with configuration

--- svg4everybody.ie8.js
+++ svg4everybody.ie8.js
@@ -1,4 +1,27 @@
-(function (document, requestAnimationFrame, CACHE, LTEIE8, IE9TO11) {
+(function (document, requestAnimationFrame, CACHE, settings) {
+	var defaults = {
+		copyAttributes: ['id', 'class', 'title'],
+		replaceExt: '.png'
+	};
+
+	for (var field in defaults) { settings[field] = typeof settings[field] == 'undefined' ? defaults[field] : settings[field]; }
+
+	if (typeof settings.fix == 'undefined') {
+		var
+		svgclippaths = !! document.createElementNS &&
+			/SVGClipPath/.test({}.toString.call(document.createElementNS('http://www.w3.org/2000/svg', 'clipPath'))),
+		LTEIE8 = /MSIE\s[1-8]\b/.test(navigator.userAgent),
+		IE9TO11 = /Trident\/[567]\b/.test(navigator.userAgent);
+
+    		settings.fix = false;
+
+    		if (!svgclippaths || LTEIE8){
+    			settings.fix = 'replace';
+    		}else if(svgclippaths || IE9TO11){
+    			settings.fix = 'svgclippaths';
+    		}
+	}
+
 	function onload() {
 		var xhr = this, x = document.createElement('x'), s = xhr.s;
 
@@ -25,16 +48,38 @@
 		var use;
 
 		while ((use = document.getElementsByTagName('use')[0])) {
-			if (LTEIE8) {
-				var
-				img = new Image();
+			var svg = use.parentNode;
 
-				img.src = use.getAttribute('xlink:href').replace('#', '.') + '.png';
-
-				use.parentNode.replaceChild(img, use);
-			} else {
+			if (settings.fix == 'replace') {
 				var
-				svg = use.parentNode,
+				div = document.createElement('div'),
+				img = new Image(),
+				attributes = ['id', 'class', 'title'];
+
+				img.src = use.getAttribute('xlink:href').replace('#', '.') + settings.replaceExt;
+
+				if (settings.copyAttributes && settings.copyAttributes.length > 0) {
+					for (var i = settings.copyAttributes.length - 1; i >= 0; i--) {
+						var
+						attributeName = settings.copyAttributes[i],
+						attributeValue = svg.getAttribute(attributeName);
+
+						/* can't use .hasAttribute in ie < 8 */
+						attributeValue = (!attributeValue || attributeValue === 'undefined' || attributeValue.length == 0) ? false : attributeValue;
+
+						if (attributeValue) {
+							div.setAttribute(attributeName, attributeValue);
+
+							if (attributeName == 'title') img.setAttribute('alt', attributeValue);
+						}
+					}
+				}
+
+				div.appendChild(img);
+
+				svg.parentNode.replaceChild(div, svg);
+			} else if(settings.fix == 'svgclippaths') {
+				var
 				url = use.getAttribute('xlink:href').split('#'),
 				url_root = url[0],
 				url_hash = url[1],
@@ -58,18 +103,17 @@
 			}
 		}
 
-		requestAnimationFrame(onframe);		
+		requestAnimationFrame(onframe);
 	}
 
-	if (LTEIE8 || IE9TO11) {
+	if (settings.fix) {
 		onframe();
 	}
 })(
 	document,
 	window.requestAnimationFrame || window.setTimeout,
 	{},
-	/MSIE\s[1-8]\b/.test(navigator.userAgent),
-	/Trident\/[567]\b/.test(navigator.userAgent),
+	typeof svg4eb == 'undefined' ? {} : svg4eb,
 	document.createElement('svg'),
 	document.createElement('use')
 );
